3e8e5e00a1886b3d2d03e0e49d420b94f2829aed
tspoff@gmail.com <tspoff@gmail.com>
2020-07-06 21:41:20 -0500

    Add build-transfer-ckb-tx, Lumos config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
-require('dotenv').config()
-const express = require('express');
-const { indexer, collectCells, collectTransactions } = require('./indexer');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { indexer, collectCells, collectTransactions } = require("./indexer");
+const { common, secp256k1Blake160, helper } = require("@ckb-lumos/common-scripts");
+const {generateAddress, parseAddress, createTransactionFromSkeleton,
+  sealTransaction, TransactionSkeleton } = require("@ckb-lumos/helpers");
+  const { initializeConfig, getConfig } = require("@ckb-lumos/config-manager");
 
 const app = express();
- 
-app.get('/get-cells', (req, res) => {
-  const cells = collectCells.then(cells => {
+app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+initializeConfig();
+
+app.post("/get-cells", (req, res) => {
+  console.log('request', req.body); // your JSON
+  const cells = collectCells(req.body).then((cells) => {
     return res.json(JSON.stringify(cells));
-  })
-});
- 
-app.get('/get-transactions', (req, res) => {
-  const transactions = collectTransactions.then(cells => {
-    return res.json(JSON.stringify(transactions));
-  })
+  });
 });
 
-app.listen(process.env.PORT, () =>
-  {
-    console.log(`ckb-indexer-node listening on port ${process.env.PORT}!`),
-      indexer.startForever();
-      collectCells({});
-  }
-);
+app.post("/get-transactions", (req, res) => {
+  const transactions = collectTransactions().then((cells) => {
+    return res.json(JSON.stringify(transactions));
+  });
+});
+
+app.post("/build-transfer-ckb-tx", async (req, res) => {
+  console.log('/build-transfer-ckb-tx', req.body);
+  const {sender, recipient, amount, txFee} = req.body;
+
+  let txSkeleton = TransactionSkeleton({ cellProvider: indexer });
+
+  txSkeleton = await secp256k1Blake160.transfer(txSkeleton, sender, recipient, BigInt(amount));
+  // console.log(JSON.stringify(createTransactionFromSkeleton(txSkeleton), null, 2));
+
+  txSkeleton = await secp256k1Blake160.payFee(txSkeleton, sender, txFee);
+  console.log(JSON.stringify(createTransactionFromSkeleton(txSkeleton), null, 2));
+
+  txSkeleton = secp256k1Blake160.prepareSigningEntries(txSkeleton);
+  console.log(txSkeleton.get("signingEntries").toArray());
+  return res.json(JSON.stringify(txSkeleton));
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`ckb-indexer-node listening on port ${process.env.PORT}!`),
+    indexer.startForever();
+});
--- src/indexer.js
+++ src/indexer.js
@@ -10,21 +10,12 @@ const indexer = new Indexer(
 );
 
 async function collectCells(params) {
-  params = {
-    lock: {
-      code_hash:
-        "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
-      hash_type: "type",
-      args: "0x27fe447b532a2cc8282aa655dec3077b7e5d83a0",
-    },
-  };
 
   const collector = new CellCollector(indexer, params);
 
   const cells = [];
   for await (const cell of collector.collect()) {
     cells.push(cell);
-    console.log(cell);
   }
 
   return cells;
@@ -36,7 +27,6 @@ async function collectTransactions(params) {
   const txs = [];
   for await (const tx of txCollector.collect()) {
     txs.push(tx);
-    console.log(tx);
   }
 
   return txs;
